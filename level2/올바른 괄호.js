cadb08a3473584a4e1a67d43973eef4e737ce5a9
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-06-13 19:54:50 +0900

    Create 올바른 괄호.js

---
+++ level2/올바른 괄호.js
@@ -0,0 +1,22 @@
+function solution(s){
+    var answer = true;
+    let stack = [];
+    for(let i = 0; i<s.length; i++){
+        if(s[i] === "(" ){
+            stack.push("(");
+        } else {
+            if(stack.length === 0){
+                answer = false;
+                return answer;
+            }
+            stack.pop();
+        }
+    }
+    if(stack.length === 0){
+        answer = true;
+    } else {
+        answer = false;
+    }
+
+    return answer;
+}
