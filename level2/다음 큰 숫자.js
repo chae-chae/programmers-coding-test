5518af37d6c1664dfae7db16a7829675c92468fb
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-03-20 12:12:36 +0900

    Update 다음 큰 숫자.js

--- level2/다음 큰 숫자.js
+++ level2/다음 큰 숫자.js
@@ -2,7 +2,7 @@ function solution(n) {
     let answer = 0;
     let oneCnt = 0;
     oneCnt = countOne(n);
-    let nextBigNum = n+1;
+    let nextBigNum = n + 1;
     let nextBigNumsOneCnt = countOne(nextBigNum);
     while(nextBigNumsOneCnt !== oneCnt){
         nextBigNum++;
@@ -13,5 +13,25 @@ function solution(n) {
 }
 
 function countOne(n){
-    return n.toString(2).toString().match(/1/g).length;
+    return n.toString(2).match(/1/g).length;
+}
+
+//
+
+function solution2(n) {
+    let answer = 0;
+    let next = n + 1;
+    let nOneCnt = 0;
+    let binCnt = 0;
+    n.toString(2).split("").forEach((c) => c === "1" ? nOneCnt++ : null );
+    next.toString(2).split("").forEach((c) => c === "1" ? binCnt++ : null );
+    while(nOneCnt !== binCnt){
+        binCnt = 0;
+        next.toString(2).split("").forEach((c) => c === "1" ? binCnt++ : null );
+        if (nOneCnt === binCnt) break;
+        next++;
+    }
+    
+    answer = next;
+    return answer;
 }
