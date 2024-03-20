809e2ff3d900f1f3ac1354e1546d5865ed5f80cc
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-03-20 12:50:47 +0900

    Update 영어 끝말잇기.js

--- level2/영어 끝말잇기.js
+++ level2/영어 끝말잇기.js
@@ -29,3 +29,24 @@ function solution(n, words) {
     }
     return answer;
 }
+
+//
+
+function solution2(n, words) {
+    let answer = [0, 0];
+    let set = new Set();
+    set.add(words[0]);
+    
+    for(let i = 1; i<words.length; i++){
+        if(set.has(words[i])){ // 중복이면 해당 사람 리턴
+            return [i%n + 1, Math.floor(i/n) + 1];
+        } else {
+            set.add(words[i]);
+        } // 마지막 알파벳과 첫번째 알파벳 비교하여 끝말잇기가 안되면 리턴
+        if(words[i-1][words[i-1].length-1] !== words[i][0]){
+            return [i%n + 1, Math.floor(i/n) + 1];
+        }
+    }
+
+    return answer;
+}
