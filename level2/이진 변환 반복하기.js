b5a774ee8b0873bba77c272e2a55d16347bb85f9
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-03-21 13:53:53 +0900

    Update 이진 변환 반복하기.js

--- level2/이진 변환 반복하기.js
+++ level2/이진 변환 반복하기.js
@@ -22,3 +22,21 @@ function solution(s) {
 
     return answer;
 }
+
+//
+
+function solution2(s) {
+    let cnt = 0;
+    let zero = 0;
+
+    while(s !== "1"){
+        cnt++;
+        zero += s.split("").filter((item) => item === "0").length;
+        let c = s.split("").filter((item) => item === "1").length;
+        s = c.toString(2);
+    }
+    
+    let answer = [cnt, zero];
+    
+    return answer;
+}
