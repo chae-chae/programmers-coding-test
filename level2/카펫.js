b98865394989528d3729f729cebcb47aabc3702a
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-03-22 10:56:49 +0900

    Update 카펫.js

--- level2/카펫.js
+++ level2/카펫.js
@@ -19,3 +19,24 @@ function solution(brown, yellow) {
 
     return answer;
 }
+
+//
+
+function solution2(brown, yellow) {
+    let answer = [];
+    let sum = brown + yellow;
+    for(let x = 1; x<=sum; x++){
+        for(let y = 1; y<=x; y++){
+            if(x*y === sum){
+                if((x-2)*(y-2) === yellow){
+                    answer[0] = x;
+                    answer[1] = y;
+                    break;
+                }
+                
+            }
+        }
+        if(answer[0] && answer[1]) break;
+    }
+    return answer;
+}
