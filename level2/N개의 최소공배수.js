1d96cf50310d02c526948f263dc8dae48a33745d
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-03-22 11:27:18 +0900

    Create N개의 최소공배수.js

---
+++ level2/N개의 최소공배수.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    let answer = arr[0];
+    for(let i = 1; i<arr.length; i++){
+        answer = lcm(answer, arr[i]);
+    }
+    return answer;
+}
+
+function gcd(a, b){
+    let remain = a % b;
+    if(remain === 0) return b;
+    return gcd(b, remain);
+}
+
+function lcm(a, b){
+    return a * b / gcd(a, b);
+}
