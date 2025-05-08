cad6e8e74f1c88e0e8955a61ed5afbdce8f84d0c
chae-chae <chaechae.d3v@gmail.com>
2025-05-08 13:52:45 +0900

    Update 폰켓몬.js

--- level1/폰켓몬.js
+++ level1/폰켓몬.js
@@ -1,14 +1,21 @@
 function solution(nums) {
-    var answer = 0;
-
-    let targetCnt = nums.length / 2;
-    let pokeCnt = new Set(nums).size;
-
-    if(targetCnt > pokeCnt) {
-        answer = pokeCnt;
-    } else {
-        answer = targetCnt;
-    }
-
-    return answer;
+    const set = new Set(nums);
+    const maxCnt = nums.length / 2;
+    
+    return Math.min(set.size, maxCnt);
 }
+
+// function solution(nums) {
+//     var answer = 0;
+
+//     let targetCnt = nums.length / 2;
+//     let pokeCnt = new Set(nums).size;
+
+//     if(targetCnt > pokeCnt) {
+//         answer = pokeCnt;
+//     } else {
+//         answer = targetCnt;
+//     }
+
+//     return answer;
+// }
