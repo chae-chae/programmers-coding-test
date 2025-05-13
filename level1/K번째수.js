f438dd22848ef50785b26a66897ff8711f08fcac
chae-chae <chaechae.d3v@gmail.com>
2025-05-13 14:08:07 +0900

    Create K번째수.js

---
+++ level1/K번째수.js
@@ -0,0 +1,7 @@
+function solution(array, commands) {
+    const answer = [];
+    for(let [i, j, k] of commands){
+        answer.push(array.slice(i-1, j).sort((a, b) => a - b)[k-1]);
+    }
+    return answer;
+}
