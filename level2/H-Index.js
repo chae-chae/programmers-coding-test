6b4394925411e35e6c7e2c7f5b3a687968fe67cb
chae-chae <chaechae.d3v@gmail.com>
2025-05-14 15:44:27 +0900

    Create H-Index.js

---
+++ level2/H-Index.js
@@ -0,0 +1,9 @@
+function solution(citations) {
+    const sorted = citations.sort((a, b) => b - a);
+    for (let i = 0; i < sorted.length; i++) {
+        if (sorted[i] < i + 1) {
+            return i;
+        }
+    }
+    return sorted.length;
+}
